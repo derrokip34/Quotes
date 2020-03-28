0f8d66d586b89084f88a3657d6847fa5fb16cddf
derrickip34 <derrickip34@gmail.com>
2020-03-28 21:02:47 +0300

    Show author of quote

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote'
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  quotes = [
+    new Quote('Do or do not there is no try', 'Yoda', 'Derrick', new Date(2020,3,28)),
+    new Quote('My only regret is that I have only one life to give to my country', 'Nathan Hale', 'Chandler', new Date(2020,3,18))
+  ]
 
   constructor() { }
 
