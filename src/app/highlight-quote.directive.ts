02b2e0ea7dc8b7c9bc90cbcb40b49e123f8f8634
derrickip34 <derrickip34@gmail.com>
2020-04-02 13:10:38 +0300

    Change hioghlight to grey

--- src/app/highlight-quote.directive.ts
+++ src/app/highlight-quote.directive.ts
@@ -6,7 +6,7 @@ import { Directive, ElementRef } from '@angular/core';
 export class HighlightQuoteDirective {
 
   constructor(private elem:ElementRef) {
-    this.elem.nativeElement.style.backgroundColor = 'blue'
+    this.elem.nativeElement.style.backgroundColor = 'grey'
    }
 
 }
